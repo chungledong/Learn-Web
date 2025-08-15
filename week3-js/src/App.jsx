a09861d09993d10d53f20d7769eb9c9b2bdc2302
Le Dong Chung <chungledong@gmail.com>
2025-08-15 21:19:26 +0700

    week3-js 21h

--- week3-js/src/App.jsx
+++ week3-js/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import UserTitle from './components/UserTitle.jsx'
+import GitHubUser from './components/GitHubUser.jsx'
 function App() {
   const [count, setCount] = useState(0)
 
@@ -15,6 +16,9 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <div className='App'>
+        <GitHubUser />
+      </div>
     </>
   )
 }
--- week3-js/src/components/GitHubUser.jsx
+++ week3-js/src/components/GitHubUser.jsx
@@ -3,34 +3,36 @@ import React ,{useState, useEffect  } from "react";
 const GitHubUser = () => {
   const [username, setUsername] = useState(null);
   const [loading, setLoading] = useState(true);
-  const [userData, setUserData] = useState(null);
 
   useEffect(() => {
-    if (username) {
-      const fetchUserData = async () => {
-        setLoading(true);
-        const response = await fetch(`https://api.github.com/users/${username}`);
+    const fetchUserData = async () => {
+      try {
+        const response = await fetch(`https://api.github.com/users/chungledong`);
         const data = await response.json();
-        setUserData(data);
-      };
-      fetchUserData();
-    }
-  }, [username]);
+        setUsername(data);
+      } catch (error) {
+        console.error("Error fetching user data:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchUserData();
+  }, []);
 
+  if (loading) {
+    return <div>Loading...</div>;
+  }
   return (
     <div>
       <h1>GitHub User Profile</h1>
-      <input
-        type="text"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-        placeholder="Enter GitHub username"
+      <img src={username.avatar_url} alt={`Avatar of ${username.login}`} 
+      style={{ width: '100px', height: '100px' } }
       />
-      {userData && (
+      {username && (
         <div>
-          <h2>{userData.name}</h2>
-          <p>{userData.bio}</p>
-          <img src={userData.avatar_url} alt={userData.name} />
+          <h2>{username.name}</h2>
+          <p>{username.bio}</p>
+          <img src={username.avatar_url} alt={username.name} />
         </div>
       )}
     </div>
